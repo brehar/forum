4a3b4d40432570dbbe8d3fdda14be85d8e8863f7
Brett Hartman <brett.hartman@mac.com>
2019-03-30 18:30:58 -0700

    Implement model associations and showing of replies to threads with tests

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -6,11 +6,20 @@
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
                     <div class="panel-heading">{{ $thread->title }}</div>
-
-                    <div class="panel-body">
-                        {{ $thread->body }}
-                    </div>
+                    <div class="panel-body">{{ $thread->body }}</div>
                 </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                @foreach ($thread->replies as $reply)
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            {{ $reply->created_at->diffForHumans() }}, <a href="#">{{ $reply->owner->name }}</a> said...
+                        </div>
+                        <div class="panel-body">{{ $reply->body }}</div>
+                    </div>
+                @endforeach
             </div>
         </div>
     </div>
