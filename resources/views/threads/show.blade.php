97a8044b163856205f8dcc44e14f4248bd02a57a
Brett Hartman <brett.hartman@mac.com>
2019-03-31 12:01:31 -0700

    Create form for replies to threads and add miscellaneous other links to app

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -19,5 +19,24 @@
                 @endforeach
             </div>
         </div>
+        @if (auth()->check())
+            <div class="row">
+                <div class="col-md-8 col-md-offset-2">
+                    <form method="POST" action="{{ $thread->path() . '/replies' }}">
+                        {{ csrf_field() }}
+                        <div class="form-group">
+                            <textarea name="body" id="body" class="form-control" placeholder="Have something to say?" rows="5"></textarea>
+                        </div>
+                        <button type="submit" class="btn btn-default">Post</button>
+                    </form>
+                </div>
+            </div>
+        @else
+            <div class="row">
+                <div class="col-md-8 col-md-offset-2">
+                    <p class="text-center">Please <a href="{{ route('login') }}">login</a> to participate in this discussion. Don't have an account? <a href="{{ route('register') }}">Register</a> now!</p>
+                </div>
+            </div>
+        @endif
     </div>
 @endsection
